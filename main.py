478c1c428c5c04dd949a82a3749f4f8eb266bf8f
Abraham <molinaabraham127@gmail.com>
2024-05-21 17:07:10 -0500

    added lines csv

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 pandas.options.display.float_format = '{:,.2f}'.format
 
 file_path_t_haliburton_regszn = 'data/t_haliburton_23-24_regszn.csv'
-file_path_t_haliburton_playoffs = 'data/t_haliburton.csv'
+file_path_t_haliburton_playoffs = 'data/t_haliburton_23-24_playoffs.csv'
 
 set1 = pandas.read_csv(file_path_t_haliburton_regszn, parse_dates=['Date'], dtype={'G': str})
 set2 = pandas.read_csv(file_path_t_haliburton_playoffs, parse_dates=['Date'], dtype={'G': str})
@@ -51,6 +51,10 @@ def preprocess(file_path):
 
 set1 = preprocess(file_path_t_haliburton_regszn)
 set2 = preprocess(file_path_t_haliburton_playoffs)
+target_data = set2.tail(1)
+set2.drop(set2.tail(1).index,inplace=True)
+
+set1 = pandas.concat([set1, set2], ignore_index=True)
 
 avg_pts = set1['PTS'].mean()
 print(set1)
@@ -79,7 +83,7 @@ plt.title('Points vs. Rebounds to Assists Ratio')
 plt.show()
 
 # model
-X = set1[['MP', 'FG%', '3P%', 'FT%', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'rebounds_assists_ratio', 'PTS']]
+X = set1[['MP', 'FG%', '3P%', 'FT%', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'rebounds_assists_ratio']]
 y = set1['above_average'].astype(int)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -100,22 +104,22 @@ print('Confusion Matrix:')
 print(confusion_matrix(y_test, y_pred))
 
 new_data = pandas.DataFrame({
-    'MP': [35],
-    'FG%': [0.45],
+    'MP': [32.2],
+    'FG%': [0.47],
     '3P%': [0.38],
     'FT%': [0.85],
-    'TRB': [5],
-    'AST': [7],
-    'STL': [1],
-    'BLK': [0],
+    'TRB': [4.5],
+    'AST': [8.5],
+    'STL': [1.2],
+    'BLK': [0.5],
     'TOV': [2],
-    'rebounds_assists_ratio': [5/7],
-    'PTS': [25]
+    'rebounds_assists_ratio': [0.67],
+    'PTS': [180.5]
 })
 
-first_row = set2.head(1).copy()
-print(set2.head(1))
-first_row_prediction = model.predict(first_row[X.columns])
+first_row_prediction = model.predict(new_data[X.columns])
 print(first_row_prediction)
 
-# print(f'Prediction: {"Above Average" if first_row_prediction[0] else "Below Average"}')
+print(f'Prediction: {"Above Average" if first_row_prediction[0] else "Below Average"}')
+print(target_data)
+#print(set1['PTS'].mean())
